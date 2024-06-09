d38087dd3ff84419ea8d86c12ac64050a6cf24aa
KnowWon <dougfeesh@hotmail.com>
2024-06-09 21:29:26 +0200

    Update app.py
    going back to closest to working version

--- scripts/app.py
+++ scripts/app.py
@@ -6,8 +6,15 @@ import wave
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('levels2.html')
+
 def get_audio_levels(filename):
     try:
+        # Wait for the audio capture to complete
+        time.sleep(1)  # Adjust sleep time as needed
+
         # Convert the file to a standard PCM format using sox
         converted_filename = "converted_input.wav"
         subprocess.run(['sox', filename, '-b', '16', '-t', 'wav', converted_filename], check=True)
@@ -38,15 +45,11 @@ def get_audio_levels(filename):
 @app.route('/audio_levels')
 def audio_levels():
     try:
-        print("Received request for audio levels")
         # Capture audio using jack_capture
         subprocess.run(['jack_capture', '-d', '1', '-c', '2', 'input2.wav'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Get audio levels from the captured file
         peak_left, peak_right = get_audio_levels('input2.wav')
-
-        print(f"Received audio levels from capture_audio_levels.py - Peak Left: {peak_left}, Peak Right: {peak_right}")
-
         if peak_left is not None and peak_right is not None:
             return jsonify({'peak_left': peak_left, 'peak_right': peak_right})
         else:
@@ -54,5 +57,13 @@ def audio_levels():
     except Exception as e:
         return jsonify({'error': f"Error: {e}"})
 
+def update_audio_levels():
+    while True:
+        time.sleep(1)
+        with app.app_context():
+            audio_levels()
+
 if __name__ == "__main__":
+    import threading
+    threading.Thread(target=update_audio_levels).start()
     app.run(debug=True)
